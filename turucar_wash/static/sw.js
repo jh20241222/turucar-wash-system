b4a9ac5851b631e40da8ba99c81779d4a50f55d2
jh20241222 <jeongyeon.kim@peoplecar.co.kr>
2026-05-13 16:15:18 +0900

    Add files via upload

---
+++ turucar_wash/static/sw.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = 'turu-wash-pwa-v1';
+const APP_SHELL = [
+  '/offline',
+  '/static/css/style.css',
+  '/static/img/turu_logo.png',
+  '/static/img/icons/icon-192.png',
+  '/static/img/icons/icon-512.png',
+  '/static/img/icons/maskable-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+  const url = new URL(request.url);
+
+  if (request.method !== 'GET' || url.origin !== self.location.origin) {
+    return;
+  }
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request).catch(() => caches.match('/offline'))
+    );
+    return;
+  }
+
+  if (url.pathname.startsWith('/static/')) {
+    event.respondWith(
+      caches.match(request).then((cached) => {
+        const networkFetch = fetch(request).then((response) => {
+          if (response && response.ok) {
+            const responseClone = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(request, responseClone));
+          }
+          return response;
+        }).catch(() => cached);
+        return cached || networkFetch;
+      })
+    );
+  }
+});

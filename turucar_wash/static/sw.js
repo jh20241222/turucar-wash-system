6fc4d19156fe61d0c027f4055d1367a1f9503cfc
jh20241222 <jeongyeon.kim@peoplecar.co.kr>
2026-07-31 11:01:59 +0900

    Add files via upload

--- turucar_wash/static/sw.js
+++ turucar_wash/static/sw.js
@@ -1,7 +1,7 @@
-const CACHE_NAME = 'turu-app-loader-v39';
+const CACHE_NAME = 'turu-app-loader-v40';
 const APP_SHELL = [
   '/offline',
-  '/static/css/style.css?v=39',
+  '/static/css/style.css?v=40',
   '/static/js/support_alerts.js',
   '/static/js/app_loader.js',
   '/static/img/turucar_logo_brand.png',
